f8b004a9bdf0e6a4b61af23cc362cc51be62bcc2
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-02-13 16:05:12 -0300

    feat: add Swiper carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Navigation, Thumbs } from "swiper";
+import { Autoplay, EffectFade } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import "swiper/swiper.css";
 
 const Home: NextPage = () => {
+  const images = [
+    "http://lorempixel.com.br/1700/500/?1",
+    "http://lorempixel.com.br/1700/500/?2",
+    "http://lorempixel.com.br/1700/500/?3",
+    "http://lorempixel.com.br/1700/500/?4",
+    "http://lorempixel.com.br/1700/500/?5",
+    "http://lorempixel.com.br/1700/500/?6",
+    "http://lorempixel.com.br/1700/500/?7",
+  ];
+
   return (
     <>
       <Head>
@@ -33,14 +45,29 @@ const Home: NextPage = () => {
 
         <section className="h-[100vh] bg-slate-500 pt-24">
           <Swiper
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            effect="fade"
+            fadeEffect={{
+              crossFade: true,
+            }}
+            speed={800}
+            centeredSlides={true}
+            modules={[Autoplay, EffectFade]}
             loop={true}
-            navigation={true}
             grabCursor={true}
-            modules={[Navigation]}
             spaceBetween={10}
             slidesPerView={1}
             className="mx-auto h-[50vh] w-[90vw] rounded-3xl border-2"
-          ></Swiper>
+          >
+            {images.map((url, index) => (
+              <SwiperSlide key={index}>
+                <img src={url} alt="" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </section>
       </main>
     </>
