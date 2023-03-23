bd82fccbb660a2c733095bcf32648e773837a737
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-03-23 16:54:37 -0300

    feat: Create a next-image component that preserve aspect ratio

---
+++ src/components/NaturalImage.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Image from "next/image";
+
+import type { FC } from "react";
+
+interface NaturalImage extends React.HTMLAttributes<HTMLImageElement> {
+  alt: string;
+  src: string;
+}
+
+const NaturalImage: FC<NaturalImage> = ({ src, alt, ...props }) => {
+  const [ratio, setRatio] = useState(16 / 9); // default to 16:9
+
+  console.log(src);
+
+  return (
+    <Image
+      {...props}
+      // set the dimension (affected by layout)
+      width={9000}
+      height={9000 / ratio}
+      /* fill */ // you can use "responsive", "fill" or the default "intrinsic"
+      alt={alt}
+      src={src}
+      placeholder="empty"
+      onLoadingComplete={({
+        naturalWidth,
+        naturalHeight,
+      }: {
+        naturalWidth: number;
+        naturalHeight: number;
+      }) => {
+        setRatio(naturalWidth / naturalHeight);
+        console.log(`W: ${naturalWidth} H: ${naturalHeight}`);
+      }}
+    />
+  );
+};
+
+export default NaturalImage;
