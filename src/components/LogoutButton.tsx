5a50ba959aea97d1cfe97715bb9abbf83915d86f
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-03-23 16:54:55 -0300

    feat: create logout button component

---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,22 @@
+import { signIn, signOut } from "next-auth/react";
+
+import type { FC } from "react";
+
+type CustomButton = React.HTMLAttributes<HTMLInputElement>;
+
+const Logout: FC<CustomButton> = ({ ...props }) => {
+  return (
+    <input
+      {...props}
+      type="button"
+      value="logout"
+      onClick={() => {
+        signOut()
+          .then(() => console.log("login"))
+          .catch((err) => console.error(err));
+      }}
+    />
+  );
+};
+
+export default Logout;
