490842d624b4782ccea58b01af6f29ae28024a76
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-03-27 20:13:43 -0300

    style: create grid styles to manage screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,16 @@ module.exports = {
       boxShadow: {
         footer: "0 -1px 2px rgba(0, 0, 0, 0.05)",
       },
+      gridTemplateColumns: {
+        manage8: "repeat(8, minmax(8rem, 1fr))",
+        manage7: "repeat(7, minmax(8rem, 1fr))",
+        manage6: "repeat(6, minmax(8rem, 1fr))",
+        manage5: "repeat(5, minmax(8rem, 1fr))",
+        manage4: "repeat(4, minmax(8rem, 1fr))",
+        manage3: "repeat(3, minmax(8rem, 1fr))",
+        manage2: "repeat(2, minmax(8rem, 1fr))",
+        manage1: "repeat(1, minmax(8rem, 1fr))",
+      },
     },
   },
   plugins: [],
