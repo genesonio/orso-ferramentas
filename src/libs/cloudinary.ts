78798b0e7d9fe0e4b0354c848d77928f3d5b91ae
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-03-28 18:16:08 -0300

    feat: instance destroy func

--- src/libs/cloudinary.ts
+++ src/libs/cloudinary.ts
@@ -1,6 +1,8 @@
 import { v2 as cloudinary } from 'cloudinary'
 
 import { env } from '../env.mjs'
+import convertBase64 from '../utils/convertBase64'
+
 
 cloudinary.config( {
   cloud_name: env.NEXT_PUBLIC_CLOUD_NAME,
@@ -9,8 +11,15 @@ cloudinary.config( {
   secure: true
 } )
 
-export const Uploader = async ( photo: string ) => {
-  const res = await cloudinary.uploader.upload( photo )
+export const Delete = async ( id: string ) => {
+  const res: { "result": string } = await cloudinary.uploader.destroy( id ) as { "result": string }
+  console.log( res )
+  return res
+}
+
+export const Uploader = async ( photo: File ) => {
+  const base64 = await convertBase64( photo )
+  const res = await cloudinary.uploader.upload( base64 as string )
     .catch( err => console.error( err ) )
   return res
 }
