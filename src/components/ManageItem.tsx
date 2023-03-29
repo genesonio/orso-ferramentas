c51cd2ab82ad1d1f8bc90ebb7361f54e1900a27a
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-03-29 17:51:39 -0300

    refact: remove /[table]/ from href, and add a conditional name rendering

--- src/components/ManageItem.tsx
+++ src/components/ManageItem.tsx
@@ -15,13 +15,14 @@ const ManageItem = ({
   return (
     <Link
       {...props}
-      href="/admin/[table]/[itemId]"
-      as={`/admin/${table}/${data.id}`}
+      href="/admin/[itemId]"
+      as={`/admin/${data.id}`}
       className="flex w-32 flex-col text-center"
     >
       <div className="w-32">
         <NaturalImage src={data.photo} alt="" />
       </div>
+      {data?.name && <h1 className="mt-2 font-bold">{data.name}</h1>}
       {data?.title && <h1 className="mt-2 font-bold">{data.title}</h1>}
       {data?.subTitle && <h2 className="mt-1 italic">{data.subTitle}</h2>}
     </Link>
